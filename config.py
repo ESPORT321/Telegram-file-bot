9ac9b1716edc6ceabc6bc03528312d8e7ef17536
ESPORT321 <nitin04359@gmail.com>
2026-06-26 09:45:44 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("8555545602:AAHyxEiPC8lygI77gbq76ukN2ff7Wrt9rFE")
+API_ID = int(os.getenv("32725610"))
+API_HASH = os.getenv("dc840d912277edc72e59705d4dd481a7")
+
+MONGO_URI = os.getenv("mongodb+srv://Shubh221:Shubh%402212009@cluster0.gemeexr.mongodb.net/telegrambot?retryWrites=true&w=majority&appName=Cluster0")
+
+ADMIN_ID = int(os.getenv("7403205087"))
+CHANNEL_USERNAME = os.getenv("@shubhcheats")
